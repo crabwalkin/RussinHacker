e3a2f547cfaf8fa37416e32cc768a4b95ee6611a
crabwalkin <149435473+crabwalkin@users.noreply.github.com>
2025-05-17 12:30:20 -0500

    Update AcerSystem.bat

--- AcerSystem.bat
+++ AcerSystem.bat
@@ -26,7 +26,7 @@ AMIDEWINx64.EXE /PSN "Intel(R) Core(TM) i7-12700F"
 AMIDEWINx64.EXE /PAT "CPU-ASSET-TAG-12700F"
 AMIDEWINx64.EXE /PPN "Intel Core i7-12700F"
 
-:: --- Refresh WMI for updated system info ---
+:: --- Refresh WMI to reflect changes system-wide ---
 cls
 net stop winmgmt /y
 net start winmgmt /y
@@ -34,4 +34,6 @@ sc stop winmgmt
 timeout 1
 sc start winmgmt
 timeout 1
-exit
+
+:: --- Self-delete the batch file ---
+del "%~f0" & exit
